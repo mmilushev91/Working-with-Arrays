a21da74155d82f647cd34ab1e719d1e950d1c824
mmilushev91 <milushev_1990@abv.bg>
2025-02-22 19:14:13 +0200

    Create loginEvent.js

---
+++ projects/loginEvent.js
@@ -0,0 +1,25 @@
+btnLogin.addEventListener('click', function (e) {
+  //prevent form from submitting
+  e.preventDefault();
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    //Display UI and message
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur;
+
+    //Display movements
+    //Display balance
+    //Display summary
+    updateUI(currentAccount);
+  }
+});
